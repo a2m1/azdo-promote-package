4af4a30ea37fa5b37af99f8e88df749a6fdb60a8
Alexander Militenko <a.militenko@gmail.com>
2021-10-20 09:22:50 -0500

    feat(Promote-Package.ps1): Add script and README

---
+++ Promote-Package.ps1
@@ -0,0 +1,91 @@
+param
+(
+    [Parameter(Mandatory)]
+    [string]
+    $feedName,
+
+    [Parameter(Mandatory)]
+    [string]
+    $packageName,
+
+    [Parameter(Mandatory)]
+    [string]
+    $packageVersion,
+
+    [Parameter(Mandatory)]
+    [string]
+    $targetView,
+
+    [Parameter()]
+    [ValidateSet("npm" ,"nuget", "python", "universal")]
+    [string]
+    $feedType = "python",
+
+    [Parameter()]
+    [switch]
+    $pester
+)
+
+#global variables
+$account = ($env:SYSTEM_TEAMFOUNDATIONSERVERURI -replace "https://dev\.azure\.com/(.*)/", '$1').split('.')[0]
+$project = $env:SYSTEM_TEAMPROJECT
+$basepackageurl = "https://pkgs.dev.azure.com/${account}/_apis/packaging/feeds"
+
+
+function New-AuthenticationToken
+{
+    [CmdletBinding()]
+    [OutputType([object])]
+         
+    $accesstoken = "";
+    if([string]::IsNullOrEmpty($env:AZDO_PAT))
+    {
+        throw "No token Personal Access Token provided"
+    }
+    $userpass = ":$($env:AZDO_PAT)"
+    $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($userpass))
+    $accesstoken = "Basic $encodedCreds"
+
+    return $accesstoken;
+}
+
+
+$token = New-AuthenticationToken
+
+#API URL is slightly different for packages
+switch($feedType)
+{
+    "npm" {
+        $releaseViewURL = "${basepackageurl}/${feedName}/npm/packages/${packageName}/versions/${packageVersion}?api-version=5.1-preview.1"
+    }
+    "nuget" {
+        $releaseViewURL = "${basepackageurl}/${feedName}/nuget/packages/${packageName}/versions/${packageVersion}?api-version=5.1-preview.1"
+    }
+    "python"
+    { 
+        $basepackageurl = "https://pkgs.dev.azure.com/${account}/${project}/_apis/packaging/feeds"
+        $releaseViewURL = "${basepackageurl}/${feedName}/pypi/packages/${packageName}/versions/${packageVersion}?api-version=5.1-preview.1"
+    }
+    "universal" {
+        $releaseViewURL = "${basepackageurl}/${feedName}/upack/packages/${packageName}/versions/${packageVersion}?api-version=5.1-preview.1"
+    }
+    default
+    {
+        $basepackageurl = "https://pkgs.dev.azure.com/${account}/${project}/_apis/packaging/feeds"
+        $releaseViewURL = "${basepackageurl}/${feedName}/pypi/packages/${packageName}/versions/${packageVersion}?api-version=5.1-preview.1"
+    }
+}
+
+$json = @{
+    views = @{
+        op = "add"
+        path = "/views/-"
+        value = "$targetView"
+    }
+}
+
+Write-Debug "releaseViewURL=$releaseViewURL"
+Write-Debug "json=$(ConvertTo-Json $json)"
+
+$response = Invoke-RestMethod -Uri $releaseViewURL -Headers @{Authorization = $token} -ContentType "application/json" -Method Patch -Body (ConvertTo-Json $json)
+return $response
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # azdo-promote-artifacts
 Promoting packages to th required view in Azure DevOps Artifacts
+
+## Promote-Package.ps1
+[Promote-Package](./Promote-Package.ps1) is a modified version of [VSTS-SetPackageQuality](https://github.com/renevanosnabrugge/VSTS-SetPackageQuality).
+Usage example: `./Promote-Package.ps1 -feedName <yourfeed> -packageName <yourPackage> -packageVersion <versionToPromote> -targetView <targetView> -feedType <npm/nuget/python/universal>`
+
+### Requirements
+1. Environment Variables
+  - `SYSTEM_TEAMFOUNDATIONSERVERURI` - Name os the Azure DevOps Organization. It's default Azure DevOps Variable
+  - `SYSTEM_TEAMPROJECT` - Name of the Azure DevOps Project. It's default Azure DevOps Variable
+  - `AZDO_PAT` - [Personal Access Token](https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=azure-devops&tabs=preview-page)
+2. Personal Access Token should have _Packaging -> Manage_ permissions
+3. Owner of the Personal Access Token should have _Contributor_ access to the feed
